dc6a74156eb99e3e552976f9e5604f4a426ea417
cluper-LEE <wls4370@gmail.com>
2022-01-31 23:28:09 +0900

    Build project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Detail from './routes/Detail'
+import Home from './routes/Home'
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/movie/:id" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/Detail.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+function Detail() {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [movie, setMovie] = useState([])
+
+  useEffect(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json()
+    setMovie(json.data.movie)
+    setLoading(false)
+  }, [])
+
+  return (
+    <div>
+      {loading ? <h1>Loading...</h1> : <h1>{movie.title}</h1>}
+      <div>
+        <img src={movie.large_cover_image} />
+      </div>
+    </div>
+  )
+}
+
+export default Detail
